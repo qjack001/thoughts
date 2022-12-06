ec22b958f5e412847639bca3d65ef025c2d96346
Jack Guinane <jackguinane@gmail.com>
2022-12-05 23:59:47 -0800

    add dates to posts

--- src/components/markdown/frontmatter/index.mjs
+++ src/components/markdown/frontmatter/index.mjs
@@ -5,11 +5,21 @@
 
 import { visit } from 'unist-util-visit'
 import { toString } from 'hast-util-to-string'
+import Git from 'git-date-extractor'
 
 export default function injectFrontmatter() {
-	return (tree, file) => {
+	return async (tree, file) => {
 
 		file.data.astro.frontmatter.layout = '../components/post-layout.astro'
+
+		const filepath = file.history[0]
+		const timestamps = await Git.getStamps({ files: filepath })
+		const timestamp = timestamps[Object.keys(timestamps)[0]]
+		
+		console.log(timestamp.created)
+		console.log(timestamp.created * 1000)
+		file.data.astro.frontmatter.created = timestamp.created * 1000
+		file.data.astro.frontmatter.lastUpdated = timestamp.modified * 1000
 
 		visit(tree, 'heading', heading => {
 			if (heading.depth == 1) {
